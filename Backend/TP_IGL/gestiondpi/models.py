91046be92f98a611dd6e31aa0d805b01dc40aa98
Merouane Meriem <lm_merouane@esi.dz>
2024-12-19 13:58:53 +0100

    Update models.py

--- Backend/TP_IGL/gestiondpi/models.py
+++ Backend/TP_IGL/gestiondpi/models.py
@@ -1,80 +1,131 @@
+
 from django.db import models
-
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.utils.translation import gettext_lazy as _
-
-from django.contrib.auth.models import Group
+from authentification.models import User
 
 
+class Patient(models.Model):
+    id_patient = models.AutoField(primary_key=True)
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+    NSS = models.CharField(max_length=20, unique=True)
+    codeQR = models.CharField(max_length=255, unique=True)
+    date_de_naissance = models.DateField()
+    adresse = models.TextField()
+    telephone = models.CharField(max_length=15)
+    mutuelle = models.CharField(max_length=100)
+    personne_a_contacter = models.CharField(max_length=100)
 
 
-from authentification.managers import UserManager
+class Medecin(models.Model):
+    id_medecin = models.AutoField(primary_key=True)
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialite = models.CharField(max_length=100)
 
 
-from rest_framework_simplejwt.tokens import RefreshToken
+
+class Infirmier(models.Model):
+        id_infirmier = models.AutoField(primary_key=True)
+        utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Laborantin(models.Model):
+    id_laborantin= models.AutoField(primary_key=True)
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Radiologue(models.Model):
+    id_radiologue = models.AutoField(primary_key=True)
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Admin(models.Model):
+    id_admin = models.AutoField(primary_key=True)
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class PharmacienHospitalier(models.Model):
+    id_pharmacienHospitalier = models.AutoField(primary_key=True)
+    utilisateur = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def valider_ordonnance(self, ordonnance):
+        ordonnance.etat_ordonnance = True
+        ordonnance.save()
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    ADMIN = 'admin'
-    PATIENT = 'patient'
-    MEDECIN = 'medecin'
-    INFIRMIER = 'infirmier'
-    PHARMACIEN = 'pharmacien'
-    LABORANTIN = 'laborantin'
-    RADIOLOGUE = 'radiologue'
-    USER = 'user'
 
-    ROLE_CHOICES = [
-        ( PATIENT, 'Patient'),
-        (MEDECIN, 'Medecin'),
-        (INFIRMIER, 'Infirmier'),
-        (PHARMACIEN, 'Pharmacien'),
-        (LABORANTIN, 'Laborantin'),
-        ( RADIOLOGUE, 'Radiologue'),
-        (ADMIN, 'Admin'),
-        (USER , 'User'),
+
+
+class DPI(models.Model):
+    id_dpi = models.AutoField(primary_key=True)
+    patient = models.OneToOneField(Patient, on_delete=models.CASCADE,default=1)
+    medecin = models.ForeignKey(Medecin, related_name="medcin", on_delete=models.CASCADE,default=1)
+    antecedents = models.TextField()
+
+
+class Consultation(models.Model):
+    id_consultation = models.AutoField(primary_key=True)
+    dpi = models.ForeignKey(DPI, related_name="consultations", on_delete=models.CASCADE)
+    date_consult = models.DateField()
+    resume = models.TextField(blank=True, null=True)
+
+class Ordonnance(models.Model):
+    id_ordonnance = models.AutoField(primary_key=True)
+    consultation = models.OneToOneField(Consultation, related_name="ordonnance", on_delete=models.CASCADE)
+    date_prescription = models.DateField()
+    etat_ordonnance = models.BooleanField(default=False)
+
+class Prescription(models.Model):
+    id_prescription = models.AutoField(primary_key=True)
+    ordonnance = models.ForeignKey(Ordonnance, related_name="prescriptions", on_delete=models.CASCADE)
+    dose = models.CharField(max_length=50)
+    duree = models.CharField(max_length=50)
+    medicament = models.OneToOneField('Medicament', related_name="prescription", on_delete=models.CASCADE,default=1)
+
+class Medicament(models.Model):
+    id_medicament= models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    description = models.TextField()
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    quantite = models.PositiveIntegerField()
+
+
+class Examen(models.Model):
+
+    TYPE_CHOICES = [
+        ('biologique', 'Biologique'),
+        ('radiologique', 'Radiologique'),
     ]
-    AUTH_PROVIDERS = {
-        'email': 'email',
-        'google': 'google',
-        'github': 'github',
-        'linkedin': 'linkedin'
-    }
-    id = models.BigAutoField(primary_key=True, editable=False)
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES, null=True)
-    email = models.EmailField(max_length=255, verbose_name=_("Email"), unique=True)
-    first_name = models.CharField(max_length=100, verbose_name=_("First Name"))
-    last_name = models.CharField(max_length=100, verbose_name=_("Last Name"))
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_verified = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True)
-    auth_provider = models.CharField(max_length=50, blank=False, null=False, default=AUTH_PROVIDERS.get('email'))
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    id_examen = models.AutoField(primary_key=True)
+    consultation = models.ForeignKey(Consultation, related_name="examens", on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    description = models.TextField()
+    date_prescription = models.DateField()
 
-    objects = UserManager()
+class BilanBiologique(models.Model):
+    id_bilanbiologique = models.AutoField(primary_key=True)
+    examen = models.OneToOneField(Examen, on_delete=models.CASCADE, related_name="bilan_biologique")
+    parametres = models.ManyToManyField('ParametreBiologique', related_name="bilans_biologiques")
+    laborantin = models.ForeignKey(Laborantin, related_name="bilanbiologiques", on_delete=models.CASCADE,default=1)
 
-    def tokens(self):
-        refresh = RefreshToken.for_user(self)
-        return {
-            "refresh": str(refresh),
-            "access": str(refresh.access_token)
-        }
-    def __str__(self):
-        return self.email
+class ParametreBiologique(models.Model):
+    id_parametrebiologique= models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    unite_mesure = models.CharField(max_length=20)
+    valeur_normale = models.CharField(max_length=100)
 
-    @property
-    def get_full_name(self):
-        return f"{self.first_name.title()} {self.last_name.title()}"
-    def assign_role(self, role_name):
-        # Make sure the role name is valid
-        if role_name in dict(self.ROLE_CHOICES).keys():
-            self.role = role_name
-            self.save()
-        else:
-            raise ValueError("Invalid role name")
+
+class ParametreBioMesure(models.Model):
+    id_parametrebiomesure = models.AutoField(primary_key=True)
+    parametre_biologique = models.ForeignKey('ParametreBiologique', on_delete=models.CASCADE, related_name="mesures")
+    valeur_mesuree = models.CharField(max_length=100)
+    date_mesure = models.DateField()
+
+class BilanRadiologique(models.Model):
+    id_bilanradiologique = models.AutoField(primary_key=True)
+    examen = models.OneToOneField(Examen, on_delete=models.CASCADE, related_name="bilan_radiologique")
+    image = models.ImageField(upload_to="radiologies/")
+    compte_rendu = models.TextField()
+    radiologue = models.ForeignKey(Radiologue, related_name="bilanradiologiques", on_delete=models.CASCADE,default=1)
+
+class Soin(models.Model):
+    id_soin = models.AutoField(primary_key=True)
+    dpi = models.ForeignKey(DPI, related_name="soins", on_delete=models.CASCADE)
+    infirmier = models.ForeignKey(Infirmier, related_name="soins", on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)  # Description du soin
+    date_soin = models.DateField()  # Date du soin
+    observation = models.TextField()
