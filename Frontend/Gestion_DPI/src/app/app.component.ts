93fcb33291420e8230543cd555de4c4cffc085a5
Meriem1551 <meriem200409@gmail.com>
2024-12-20 13:55:53 +0100

    adding routes

--- Frontend/Gestion_DPI/src/app/app.component.ts
+++ Frontend/Gestion_DPI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
-
+import {RecherchePatientComponent} from "./recherche-patient/recherche-patient.component";
+import { LandingPageComponent } from './landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RecherchePatientComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/Gestion_DPI/src/app/app.routes.ts
+++ Frontend/Gestion_DPI/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+    {path: '', component: LandingPageComponent },
+    {path: 'login', component: LoginComponent },
 
-export const routes: Routes = [];
+];
