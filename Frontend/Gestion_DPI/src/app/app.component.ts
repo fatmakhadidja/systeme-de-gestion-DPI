9ef91fab98154c59f5a31c20769958536fdcfb92
Meriem1551 <meriem200409@gmail.com>
2024-12-20 13:58:56 +0100

    remoing unused components

--- Frontend/Gestion_DPI/src/app/app.component.ts
+++ Frontend/Gestion_DPI/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
-import {RecherchePatientComponent} from "./recherche-patient/recherche-patient.component";
-import { LandingPageComponent } from './landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RecherchePatientComponent, LandingPageComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
