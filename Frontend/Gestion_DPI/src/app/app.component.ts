8cfcc6eb82aa4cf604109a7907f31951cb03324d
AGAL Imene <mi_agal@esi.dz>
2024-12-20 16:58:58 +0100

    Sauvegarde des soins ajoutée

--- Frontend/Gestion_DPI/src/app/app.component.ts
+++ Frontend/Gestion_DPI/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { PageInfirmierComponent } from './page-infirmier/page-infirmier.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PageInfirmierComponent],
+  imports: [PageInfirmierComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
