1b1882884f53a13cbfa5f6ab20c6fd4d0187d359
AGAL Imene <mi_agal@esi.dz>
2024-12-20 15:48:28 +0100

    Necessary edits for merging

--- Frontend/Gestion_DPI/src/app/app.component.ts
+++ Frontend/Gestion_DPI/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PageInfirmierComponent } from './page-infirmier/page-infirmier.component';
-import { HeaderComponent } from './header-component/header.component';
-import { CreationConsultationComponent } from "./creation-consultation/creation-consultation.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
-  selector: 'app-root', // the selector <app-root></app-root> is used to insert this component
-  imports: [PageInfirmierComponent], //CreationConsultationComponent
+  selector: 'app-root',
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Gestion_DPI';
 }
