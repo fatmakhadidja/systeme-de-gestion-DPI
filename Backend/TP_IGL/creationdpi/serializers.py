ea3aac65d89060c156731ac08c315b47a708b0b1
Merouane Meriem <lm_merouane@esi.dz>
2024-12-21 20:32:25 +0100

    Update serializers.py

--- Backend/TP_IGL/creationdpi/serializers.py
+++ Backend/TP_IGL/creationdpi/serializers.py
@@ -96,3 +96,16 @@ class DPICreationSerializer(serializers.Serializer):
         dpi.save()
 
         return dpi
+
+
+class QRCodeSerializer(serializers.ModelSerializer):
+    qr_code_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = DPI
+        fields = ['id_dpi', 'qr_code_url']
+
+    def get_qr_code_url(self, obj):
+        if obj.qr_code:
+            return obj.qr_code.url
+        return None
