e596a72d2c89c8d9774f655687d1e5b54d40abc3
Merouane Meriem <lm_merouane@esi.dz>
2024-12-23 18:41:01 +0100

    Update urls.py

--- Backend/TP_IGL/creationdpi/urls.py
+++ Backend/TP_IGL/creationdpi/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from .views import DPICreationView ,QRCodeView 
+
+from .views import  SearchDPIByNSSView ,QRCodeScanView
+
+from .views import DPICreationView ,QRCodeView , DPIListView ,DPIDetailView
+
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('create-dpi/', DPICreationView.as_view(), name='create_dpi'),
     path('<int:dpi_id>/qrcode/', QRCodeView.as_view(), name='qr_code_download'),
+
+    path('NssSearch/', SearchDPIByNSSView.as_view(), name='search-dpi-by-nss'),
+    path('QRCodeSearch/', QRCodeScanView.as_view(), name='qr-code-search'),
+
+    path('dpis/', DPIListView.as_view(), name='dpi-list'),
+    path('dpis/<int:pk>/', DPIDetailView.as_view(), name='dpi-detail'),
+
+
     
 ]
 if settings.DEBUG:
