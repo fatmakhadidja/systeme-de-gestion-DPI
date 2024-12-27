923d8c958d9af096d38fd2f135305019aa69034e
Meriem1551 <meriem200409@gmail.com>
2024-12-27 19:40:54 +0100

    routing to landing page

--- Frontend/Gestion_DPI/src/app/app.component.ts
+++ Frontend/Gestion_DPI/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PageLaborantinComponent } from './page-laborantin/page-laborantin.component';
 @Component({
   selector: 'app-root',
-  imports: [/*RouterOutlet,*/ PageLaborantinComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
